0828e0e86552f6a080b72f382a504dbdecde5161
John Eipe <johne@INLM-JohnE.local>
2020-02-04 14:08:52 +0530

    Contact Form and Send Email

--- basic/index.js
+++ basic/index.js
@@ -23,5 +23,37 @@ app.get('/about', function(req, res){
     res.render('about', {title:'About'});
 });
 
+app.get('/contact', function(req, res){
+    res.render('contact', {title:'Contact'});
+});
+
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'your@email.com',
+			pass: 'password'
+		}
+	});
+
+	var mailOptions = {
+		from: 'John Eipe <john77eipe@gmail.com>',
+		to: 'support@gmail.com',
+		subject: 'Website Submission',
+		text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
+});
+
 app.listen(3000);
 console.log("Server is running on port 3000");
