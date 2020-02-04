fd8606520929522a1ca03c764daec31b46b2efc9
John Eipe <johne@INLM-JohnE.local>
2020-02-04 12:22:56 +0530

    Jade(Pug) Hello World

--- basic/Readme.md
+++ basic/Readme.md
@@ -14,3 +14,6 @@ What are middleware in express? What does `app.use` do?
 http://expressjs.com/en/guide/writing-middleware.html
 https://stackoverflow.com/questions/11321635/nodejs-express-what-is-app-use
 
+Why `body-parser`?
+To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser.
+`body-parser` extracts the entire body portion of an incoming request stream and exposes it on req.body.
--- basic/index.js
+++ basic/index.js
@@ -5,11 +5,16 @@ var nodeMailer = require('nodemailer');
 
 var app = express();
 
+//specifying the location of template/view files and 
+//setting the view rendering engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res){
-    res.send('Hello world');
+    res.render('home');
 });
 app.listen(3000);
 console.log("Server is running on port 3000");
