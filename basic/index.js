5271988f6d6667399a12a7b5f90917977fdc50f0
John Eipe <johne@INLM-JohnE.local>
2020-02-04 12:51:29 +0530

    More templates and CSS

--- basic/index.js
+++ basic/index.js
@@ -9,12 +9,19 @@ var app = express();
 //setting the view rendering engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+//specifying the location for static files
+app.use(express.static(path.join(__dirname, 'public')));
+//specifying automatic parsing of POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res){
-    res.render('home');
+    res.render('home', {title:'Welcome'});
 });
+
+app.get('/about', function(req, res){
+    res.render('about', {title:'About'});
+});
+
 app.listen(3000);
 console.log("Server is running on port 3000");
